3320a41485d43f53fee78e75bee6c19a9ee60fc5
freeweb3 <166019866+freeweb3@users.noreply.github.com>
2024-04-05 09:04:58 +1030

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+{
+  "name": "LearnToBeBetter",
+  "website": "https://twitter.com/LearnToBeBetter",
+  "description": "Learn to be better",
+  "logo": "https://raw.githubusercontent.com/freeweb3/learnjs/main/logo.png",
+  "twitter": "https://twitter.com/LearnToBeBetter"
+}
